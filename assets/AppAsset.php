61b7b1c3ec3dd8a793ff4c8e00dfbb1869d17750
jamespakorn <jameshosxp@hotmail.com>
2016-10-19 11:18:21 +0700

    New Project

---
+++ assets/AppAsset.php
@@ -0,0 +1,30 @@
+<?php //class จัดการ libary ที่เพิ่มสีพื้นหลัง
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        'css/bootstrap.min_blue.css'  // bootstap ที่สร้างใหม่
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
